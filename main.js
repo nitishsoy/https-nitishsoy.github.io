c3c0c5ab14a8fc5f7838948b47e71cb23cfcb02a
nitishsoy <64145677+nitishsoy@users.noreply.github.com>
2020-07-25 13:06:38 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,46 +1,41 @@
 /// Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyBNOzSGs-hW4N4JD_-fTLXyV1wfNySVPVo",
-    authDomain: "contact-form-d72a3.firebaseapp.com",
-    databaseURL: "https://contact-form-d72a3.firebaseio.com",
-    projectId: "contact-form-d72a3",
-    storageBucket: "contact-form-d72a3.appspot.com",
-    messagingSenderId: "151559915079",
-    appId: "1:151559915079:web:afd91cf57012afe8389362",
-    measurementId: "G-XPDHH4P67V"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.database();
-
-    var contactRef = firebase.database().ref().child('contactform');
-document.getElementById('contactform').addEventListener('submit',submitform);
-
-function submitform(e){
-  e.preventDefault();
-  var Name = getInputVal('Name');
-  var Email = getInputVal('Email');
-  var Phone = getInputVal('Phone');
-  var Message = getInputVal('Message');
+var firebaseConfig = {
+   apiKey: "AIzaSyBNOzSGs-hW4N4JD_-fTLXyV1wfNySVPVo",
+   authDomain: "contact-form-d72a3.firebaseapp.com",
+   databaseURL: "https://contact-form-d72a3.firebaseio.com",
+   projectId: "contact-form-d72a3",
+   storageBucket: "contact-form-d72a3.appspot.com",
+   messagingSenderId: "151559915079",
+   appId: "1:151559915079:web:afd91cf57012afe8389362",
+   measurementId: "G-XPDHH4P67V"
+ };
+ // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+ firebase.analytics();
 
 
-  saveForm(Name,Email,Phone,Message);
-  document.getElementbyId('contactform').reset();
+  var messagesRef = firebase.database().ref('messages');
+  document.getElementById('contactform').addEventListener('submit', submitForm);
+
+function submitForm(e){
+	e.preventDefault();
+	var Name = getInputVal('Name');
+		var Email = getInputVal('Email');message
+	var Phone = getInputVal('Phone');
+	var Message = getInputVal('Message');
+	saveMessage( Name,Email,Phone,Message);
+
 }
-
-
-function getInputVal(id){
-  return document.getElementbyId(id).value;
+function getInputVal(id) {
+	return document.getElementById(id).value;
+	// body...
 }
-
-
-function saveForm(Name,Email,Phone,Message){
-  var newcontactRef = contactRef.push();
-  newcontactRef.set({
-
-    Name : Name,
-    Email : Email,
-    Phone : Phone,
-    Message : Message
-  });
+function saveMessage(first_name,last_name,email,telephone,comments){
+	var newMessageRef = messagesRef.push();
+	newMessageRef.set({
+		Name:Name,
+		Email:Email,
+		Phone:Phone,
+	Message:Message;
+	});
 }
