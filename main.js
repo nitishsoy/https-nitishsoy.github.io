4f43ebcdf12a04985ab580b3927f6549e43a8804
nitishsoy <64145677+nitishsoy@users.noreply.github.com>
2020-07-25 13:09:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ var firebaseConfig = {
  };
  // Initialize Firebase
  firebase.initializeApp(firebaseConfig);
- firebase.analytics();
+ firebase.database();
 
 
   var messagesRef = firebase.database().ref('messages');
